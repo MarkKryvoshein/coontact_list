e6efbf6deb35f4e3574b2873ff35864e129af14e
MarkKryvoshein <krivosheinmark01@gmail.com>
2025-04-12 14:10:45 +0300

    New branch feature

--- training_app/static/js/script.js
+++ training_app/static/js/script.js
@@ -1,4 +1,18 @@
+//app/static/js/main.js
 document.addEventListener("DOMContentLoaded", function() {
+
+//  window.toggleDetails = function(row) {
+//    const contactId = row.dataset.contactId;
+//    const detailsRow = document.getElementById('details-' + contactId);
+//
+//    if (detailsRow.style.display === 'none') {
+//        detailsRow.style.display === 'table-row';
+//    } else {
+//        detailsRow.style.display === 'none';
+//    }
+//  }
+
+
   let selectedRow = null;
   let selectedContactId = null;
 
@@ -18,22 +32,21 @@ document.addEventListener("DOMContentLoaded", function() {
       editButton.href = `edit/${selectedContactId}/`
 
     });
+
+    row.addEventListener("onmouseover", function() {
+        const contactId = row.dataset.contactId;
+        const detailsRow = document.getElementBпId('details-' + contactId);
+
+        if (detailsRow.style.display === 'none') {
+            detailsRow.style.display === 'table-row';
+        } else {
+            detailsRow.style.display === 'none';
+        }
+    }
+    }
+
   });
 
-  window.toggleDetails = function(row){
-    const contactId = row.dataset.contactId;
-    const detailsRow = document.getElementById('details-' + contactId);
-
-    if (detailsRow.style.display === 'none'){
-        detailsRow.style.display === 'table-row';
-    }
-    else{
-        detailsRow.style.display === 'none';
-    }
-
-
-
-  }
 
 
 
